c4f49f92a22335d35c7d3c4e66efd805536a2cfb
Juan Becerra <juanpbrizo@gmail.com>
2022-11-02 16:28:59 -0500

    Add request to database to create user

--- controllers/postUsers.js
+++ controllers/postUsers.js
@@ -1,15 +1,31 @@
 const createHttpError = require('http-errors')
-// const { User } = require('../database/models')
+const bcryptjs = require('bcryptjs')
+const { User } = require('../database/models')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
 // example of a controller. First call the service, then build the controller method
 const postUsers = catchAsync(async (req, res, next) => {
+  const { firstName, lastName, email, password } = req.body
+
+  // Encriptar contraseña
+  const salt = bcryptjs.genSaltSync()
+  const passwordBcrypt = bcryptjs.hashSync(password, salt)
+
+  const user = {
+    firstName,
+    lastName,
+    email,
+    password: passwordBcrypt
+  }
+
   try {
+    const response = await User.create(user)
+
     endpointResponse({
       res,
       message: 'Desde postUsers',
-      body: {}
+      body: response
     })
   } catch (error) {
     const httpError = createHttpError(
