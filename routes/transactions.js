94122fb12ad4ed109b0bf1e95cb6610a4a84be99
Adrian Turek <adrianbturek@gmail.com>
2022-11-02 15:44:08 -0300

    add the transactions route and create the router

---
+++ routes/transactions.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const {
+   get,
+} = require('../controllers/transactions')
+
+const router = express.Router()
+
+router.get('/', get)
+
+module.exports = router
