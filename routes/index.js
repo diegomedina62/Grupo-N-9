9ea3868262353b6aedb6e3c6aff8edc089168e7f
Sheice <75450007+Sheice@users.noreply.github.com>
2022-11-02 15:29:18 -0300

    creating router of create transaction

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const usersRouter = require('./users')
+const transactionsRouter = require('./transaction')
 
 const router = express.Router()
 
 // example of a route with index controller get function
 router.use('/users', usersRouter)
+
+// router of transaction post and update
+router.use('/transactions', transactionsRouter)
 
 module.exports = router
