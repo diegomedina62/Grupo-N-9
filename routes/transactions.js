a4f18ed64965def5daa86505a63eab33b960a523
Adrian Turek <adrianbturek@gmail.com>
2022-11-03 14:37:54 -0300

    middleware for  check id transaction

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const { getTransaction, getTransactionById } = require('../controllers/transactions')
+const checkIdTransaction = require('../middlewares/checkidtransaction')
 
 const router = express.Router()
 
 router.get('/', getTransaction)
-router.get('/:id', getTransactionById)
+router.get('/:id', checkIdTransaction, getTransactionById)
 
 module.exports = router
