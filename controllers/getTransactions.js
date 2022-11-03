96e43e00c134c1b17f23083361244416bedd6c5c
Adrian Turek <adrianbturek@gmail.com>
2022-11-03 07:13:10 -0300

    ID 13 - transaction query by id

--- controllers/getTransactions.js
+++ controllers/getTransactions.js
@@ -4,21 +4,39 @@ const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
 
 const getTransaction = catchAsync(async (req, res, next) => {
-    try {
-      const response = await Transaction.findAll()
-      endpointResponse({
-        res,
-        message: 'Transactions retrieved successfully',
-        body: response,
-      })
-    } catch (error) {
-      const httpError = createHttpError(
-        error.statusCode,
-        `[Error retrieving transactions] - [index - GET]: ${error.message}`,
-      )
-      next(httpError)
-    }
-  })
+  try {
+    const response = await Transaction.findAll()
+    endpointResponse({
+      res,
+      message: 'Transactions retrieved successfully',
+      body: response
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `[Error retrieving transactions] - [index - GET]: ${error.message}`
+    )
+    next(httpError)
+  }
+})
+
+const getTransactionById = catchAsync(async (req, res, next) => {
+  try {
+    const { id } = req.params
+    const response = await Transaction.findByPk(id)
+    endpointResponse({
+      res,
+      message: 'Transaction retrieved successfully',
+      body: response
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `[Error retrieving transactions] - [index - GET]: ${error.message}`
+    )
+    next(httpError)
+  }
+})
 
 // example of a controller. First call the service, then build the controller method
-module.exports = {getTransaction}
+module.exports = { getTransaction, getTransactionById }
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const {getTransaction} = require('../controllers/getTransactions')
+const { getTransaction, getTransactionById } = require('../controllers/getTransactions')
 
 const router = express.Router()
 
 router.get('/', getTransaction)
+router.get('/:id', getTransactionById)
 
 module.exports = router
