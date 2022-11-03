33fc3d0288884ddcac38fa21dc1bd21132ba0f0a
Juan Becerra <juanpbrizo@gmail.com>
2022-11-03 17:40:41 -0500

    Solution - bud fixed

--- controllers/users.js
+++ controllers/users.js
@@ -1,98 +1,98 @@
-const createHttpError = require('http-errors');
-const bcryptjs = require('bcryptjs');
+const createHttpError = require('http-errors')
+const bcryptjs = require('bcryptjs')
 
-const { User } = require('../database/models');
+const { User } = require('../database/models')
 
-const { endpointResponse } = require('../helpers/success');
-const { catchAsync } = require('../helpers/catchAsync');
+const { endpointResponse } = require('../helpers/success')
+const { catchAsync } = require('../helpers/catchAsync')
 
 const getUser = catchAsync(async (req, res, next) => {
-    try {
-        const response = await User.findAll();
-        endpointResponse({
-            res,
-            message: 'All users',
-            body: response,
-        });
-    } catch (error) {
-        const httpError = createHttpError(
-            error.statusCode,
+  try {
+    const response = await User.findAll()
+    endpointResponse({
+      res,
+      message: 'All users',
+      body: response
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
             `[Error retrieving users] - [index - GET]: ${error.message}`
-        );
-        next(httpError);
-    }
-});
+    )
+    next(httpError)
+  }
+})
 
 const getUserId = catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        const response = await User.findByPk(id);
-        endpointResponse({
-            res,
-            message: 'User Id succesfully',
-            body: response,
-        });
-    } catch (error) {
-        const httpError = createHttpError(
-            error.statusCode,
+  const { id } = req.params
+  try {
+    const response = await User.findByPk(id)
+    endpointResponse({
+      res,
+      message: 'User Id succesfully',
+      body: response
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
             `[Error retrieving users] - [index - GET]: ${error.message}`
-        );
-        next(httpError);
-    }
-});
+    )
+    next(httpError)
+  }
+})
 
 const postUsers = catchAsync(async (req, res, next) => {
-    const { firstName, lastName, email, password } = req.body;
+  const { firstName, lastName, email, password } = req.body
 
-    // Encriptar contraseña
-    const salt = bcryptjs.genSaltSync();
-    const passwordBcrypt = bcryptjs.hashSync(password, salt);
+  // Encriptar contraseña
+  const salt = bcryptjs.genSaltSync()
+  const passwordBcrypt = bcryptjs.hashSync(password, salt)
 
-    const user = {
-        firstName,
-        lastName,
-        email,
-        password: passwordBcrypt,
-    };
+  const user = {
+    firstName,
+    lastName,
+    email,
+    password: passwordBcrypt
+  }
 
-    try {
-        const response = await User.create(user);
+  try {
+    const response = await User.create(user)
 
-        endpointResponse({
-            res,
-            message: 'Desde postUsers',
-            body: response,
-        });
-    } catch (error) {
-        const httpError = createHttpError(
-            error.statusCode,
+    endpointResponse({
+      res,
+      message: 'Desde postUsers',
+      body: response
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
             `[Error retrieving users] - [index - GET]: ${error.message}`
-        );
-        next(httpError);
-    }
-});
+    )
+    next(httpError)
+  }
+})
 
 const deleteUser = catchAsync(async (req, res, next) => {
-    const { id } = req.params;
-    try {
-        await User.destroy({ where: { id } });
-        endpointResponse({
-            res,
-            message: 'User Id succesfully',
-            body: response,
-        });
-    } catch (error) {
-        const httpError = createHttpError(
-            error.statusCode,
+  const { id } = req.params
+  try {
+    const response = await User.destroy({ where: { id } })
+    endpointResponse({
+      res,
+      message: 'User Id succesfully',
+      body: response
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
             `[Error retrieving users] - [index - GET]: ${error.message}`
-        );
-        next(httpError);
-    }
-});
+    )
+    next(httpError)
+  }
+})
 
 module.exports = {
-    getUser,
-    getUserId,
-    postUsers,
-    deleteUser,
-};
+  getUser,
+  getUserId,
+  postUsers,
+  deleteUser
+}
