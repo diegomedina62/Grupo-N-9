8ad8647863f2a74ada9bf6e5ca0d69c2cac16703
cicarelligiuliano <cicarelligiuliano@outlook.com.ar>
2022-11-03 19:16:03 -0300

    Arreglando bugs

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
+const express = require('express')
 
-const {Router} = require('express')
-const usersRouter = require("./users");
+const usersRouter = require('./users')
 const transactionsRouter = require('./transactions')
 const categoriesRouter = require('./categories')
 
-
-const router = Router()
+const router = express.Router()
 
 // example of a route with index controller get function
 
-router.use("/users", usersRouter);
+router.use('/users', usersRouter)
 router.use('/transactions', transactionsRouter)
 router.use('/categories', categoriesRouter)
-
 
 module.exports = router
