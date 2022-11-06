1945fbe37d4b785240b13ec4597beb0f9a2cf8e7
diegomedina62 <diegoleonmedina62@gmail.com>
2022-11-06 20:39:07 -0300

    implement middleware on POST categories route

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,15 @@
-const {Router} = require('express')
-const {createCategory, getCategoryById} = require('../controllers/categories')
-const checkIdCategory = require('../middlewares/checkIdCategory')
+const { Router } = require("express");
+const {
+  createCategory,
+  getCategoryById,
+} = require("../controllers/categories");
+const checkIdCategory = require("../middlewares/checkIdCategory");
+const validationMiddleware = require("../middlewares/ValidationMiddleware");
+const categoriesSchemaPOST = require("../schemas/categoriesSchema-POST");
 
-const router = Router()
+const router = Router();
 
-router.post('/', createCategory)
-router.get('/:id', checkIdCategory, getCategoryById )
+router.post("/", validationMiddleware(categoriesSchemaPOST), createCategory);
+router.get("/:id", checkIdCategory, getCategoryById);
 
-
-module.exports = router
+module.exports = router;
