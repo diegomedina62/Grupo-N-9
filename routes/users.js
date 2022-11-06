6df00ffdfa51a11fd4ab2ab059a1c0ee114b5691
diegomedina62 <diegoleonmedina62@gmail.com>
2022-11-06 20:14:03 -0300

    implementation of middleware on PUT user route

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,31 @@
-const express = require('express')
-const validationMiddleware = require('../middlewares/ValidationMiddleware')
-const userSchemaPOST = require('../schemas/userSchema-Post')
+const express = require("express");
+const validationMiddleware = require("../middlewares/ValidationMiddleware");
+const userSchemaPOST = require("../schemas/userSchema-Post");
+const userSchemaPUT = require("../schemas/userSchema-PUT");
 
 const {
   getUser,
   getUserId,
   postUsers,
   putUsers,
-  deleteUser
-} = require('../controllers/users')
+  deleteUser,
+} = require("../controllers/users");
 
-const checkEmailUsers = require('../middlewares/checkEmailUsers')
-const checkIdUsers = require('../middlewares/checkIdUsers')
+const checkEmailUsers = require("../middlewares/checkEmailUsers");
+const checkIdUsers = require("../middlewares/checkIdUsers");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', getUser)
-router.get('/:id', checkIdUsers, getUserId)
+router.get("/", getUser);
+router.get("/:id", checkIdUsers, getUserId);
 
-router.post('/', validationMiddleware(userSchemaPOST), checkEmailUsers, postUsers)
-router.put('/:id', checkIdUsers, putUsers)
-router.delete('/:id', checkIdUsers, deleteUser)
+router.post(
+  "/",
+  validationMiddleware(userSchemaPOST),
+  checkEmailUsers,
+  postUsers
+);
+router.put("/:id", validationMiddleware(userSchemaPUT), checkIdUsers, putUsers);
+router.delete("/:id", checkIdUsers, deleteUser);
 
-module.exports = router
+module.exports = router;
