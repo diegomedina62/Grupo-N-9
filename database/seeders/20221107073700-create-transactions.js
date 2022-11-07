2da7f3159d4b89ec376af799ccb7184bf7ea69b2
Maxwell <neoformax@gmail.com>
2022-11-07 11:32:12 -0400

    completed the functionality of transactions seed

--- database/seeders/20221107073700-create-transactions.js
+++ database/seeders/20221107073700-create-transactions.js
@@ -2,15 +2,30 @@
 
 const { faker } = require('@faker-js/faker')
 
+function randomNumber (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const transactions = [
-    ]
-    for (let i = 10; i < 10; i++) {
+    const transactions = []
+    let category = 0
+    for (let i = 0; i < 20; i++) {
+      const money = randomNumber(-500, 500)
+      if (money < 0) {
+        category = 2
+      } else {
+        category = 1
+      }
+
       transactions.push({
-        description: faker.animal.cat(),
-        amount: faker.datatype.number(),
-        date: faker.date.past(),
+        description: faker.lorem.sentence(5),
+        amount: money,
+        date: faker.date.past(5),
+        userId: randomNumber(1, 20),
+        categoryId: category,
         createdAt: new Date(),
         updatedAt: new Date()
       })
