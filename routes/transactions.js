5eff5114da94faaea71a5e58551608b92f498267
diegomedina62 <diegoleonmedina62@gmail.com>
2022-11-06 22:16:40 -0300

    implementation of middleware on transaction POST

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,17 +1,23 @@
-const express = require('express');
+const express = require("express");
 const {
-    getTransaction,
-    getTransactionById,
-    createTransaction,
-    editTransaction,
-} = require('../controllers/transactions');
-const checkIdTransaction = require('../middlewares/checkidtransaction');
+  getTransaction,
+  getTransactionById,
+  createTransaction,
+  editTransaction,
+} = require("../controllers/transactions");
+const checkIdTransaction = require("../middlewares/checkidtransaction");
+const validationMiddleware = require("../middlewares/ValidationMiddleware");
+const transactionSchemaPOST = require("../schemas/transactionSchema-POST");
 
 const router = express.Router();
 
-router.get('/', getTransaction);
-router.get('/:id', checkIdTransaction, getTransactionById);
-router.post('/', createTransaction);
-router.put('/:id', editTransaction);
+router.get("/", getTransaction);
+router.get("/:id", checkIdTransaction, getTransactionById);
+router.post(
+  "/",
+  validationMiddleware(transactionSchemaPOST),
+  createTransaction
+);
+router.put("/:id", editTransaction);
 
 module.exports = router;
