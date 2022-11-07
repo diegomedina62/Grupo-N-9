8300313b514720cc7139fc61f1204f83361f280c
Maxwell <neoformax@gmail.com>
2022-11-07 01:11:30 -0400

    configure the role seed

--- database/seeders/20221107050224-create-roles.js
+++ database/seeders/20221107050224-create-roles.js
@@ -1,24 +1,16 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    const roles = [
+      { name: 'STANDARD', description: 'This is a standard rol', createdAt: new Date(), updatedAt: new Date() },
+      { name: 'REGULAR', description: 'this is a regular rol', createdAt: new Date(), updatedAt: new Date() }
+    ]
+
+    await queryInterface.bulkInsert('Roles', roles, {})
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Roles', null, {})
   }
-};
+}
