9f2a8ac668a2ebc6994c9e8dfc114f826c2e7809
Juan Becerra <juanpbrizo@gmail.com>
2022-11-07 15:16:41 -0500

    Fix JWT

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,49 +1,60 @@
-const { token } = require("morgan");
-const { putUsers } = require("../controllers/users");
-const { ErrorObject } = require("../helpers/error");
-const { User } = require('../database/models')
-const bcrypt = require('bcryptjs')
-const tokenCreator = require('../helpers/tokenCreator')
 const jwt = require('jsonwebtoken')
+const bcryptjs = require('bcryptjs')
+const { User } = require('../database/models')
+const tokenCreator = require('../helpers/tokenCreator')
+const { ErrorObject } = require('../helpers/error')
 
 const encode = async (req, res, next) => {
-
-
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ where: { email } });
+  const { email, password } = req.body
+  try {
+    const user = await User.findOne({ where: { email } })
 
     if (user === null) {
-        throw new ErrorObject("Invalid User or Password")
+      const httpError = new ErrorObject('Invalid User or Password', 404)
+      next(httpError)
     }
 
-    const compare = bcrypt.compareSync(password, user.password);
+    const compare = bcryptjs.compareSync(password, user.password)
 
     if (!compare) {
-        throw new ErrorObject("Invalid User or Password")
+      const httpError = new ErrorObject('Invalid User or Password', 404)
+      next(httpError)
     }
-
-    const token = tokenCreator(user.id)
+    const payload = { id: user.id }
+    const token = await tokenCreator(payload)
     req.token = token
 
     next()
+  } catch (error) {
+    const httpError = new ErrorObject(error.message, error.statusCode)
+    next(httpError)
+  }
 }
 
 const decode = async (req, res, next) => {
+  const token = req.headers['x-access-token']
+  if (!token) {
+    const httpError = new ErrorObject("Token Doesn't Exist")
+    next(httpError)
+  }
 
-    const token = req.headers['x-access-token'];
-    if (!token) {
-        throw new ErrorObject("Token Doesn't Exist")
+  try {
+    const { id } = jwt.verify(token, process.env.SECRETORPRIVATEKEY)
+
+    const userAuth = await User.findByPk(id)
+
+    if (userAuth === null) {
+      const httpError = new ErrorObject('Invalid Token', 400)
+      next(httpError)
     }
 
-    try {
-        const decodificado = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
-        req.userid = decodificado.id
-    } catch (error) {
-        throw new ErrorObject("Invalid Token", 400)
-    }
+    req.userAuth = userAuth
+  } catch (error) {
+    const httpError = new ErrorObject('Invalid Token', 400)
+    next(httpError)
+  }
 
-    next()
+  next()
 }
 
 module.exports = { encode, decode }
