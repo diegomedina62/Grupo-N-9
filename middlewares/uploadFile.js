63305b696f53319809507eaadef1a8acd981ae4b
Juan Becerra <juanpbrizo@gmail.com>
2022-11-06 21:22:40 -0500

    Adding extension restriction

--- middlewares/uploadFile.js
+++ middlewares/uploadFile.js
@@ -1,10 +1,19 @@
-const multer = require('multer')
+const multer = require('multer');
+const { ErrorObject } = require('../helpers/error');
 
 const filesUpload =[
     { name: 'avatar', maxCount: 1 }
 ]
+const MIMETYPES = ['image/jpeg', 'image/png', 'image/svg+xml', 'image/webp'];
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ 
+    dest: 'uploads/',
+    fileFilter: (req, file, cb) => {
+        (MIMETYPES.includes(file.mimetype)) ? 
+            cb(null, true) :
+            cb(new ErrorObject(`Only extensions are allowed: [${MIMETYPES.join(' - ')}]`, 400))
+    }
+})
 
 const cpUpload = upload.fields(filesUpload)
 
