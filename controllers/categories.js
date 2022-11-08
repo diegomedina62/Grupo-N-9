d614ca46f6266a7f40fe6b885cf1a02cb6ebf5db
Maxwell Salazar <neoformax@gmail.com>
2022-11-08 08:14:41 -0400

    21 put categories (#35)
    * create the put category
    
    * configure the route put category

--- controllers/categories.js
+++ controllers/categories.js
@@ -2,6 +2,7 @@ const createHttpError = require('http-errors')
 const { Category } = require('../database/models')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
+const validationDb = require('../helpers/validationDb')
 
 const getCategories = catchAsync(async (req, res, next) => {
   try {
@@ -64,4 +65,29 @@ const getCategoryById = catchAsync(async (req, res, next) => {
   }
 })
 
-module.exports = { createCategory, getCategoryById, getCategories }
+const editCategory = catchAsync(async (req, res, next) => {
+  const { id } = req.params
+  const { name, description } = req.body
+
+  // Validate the id
+  const schemaId = { where: { id } }
+  const category = await validationDb(schemaId, Category, true)
+
+  await category.update({ name, description })
+
+  try {
+    endpointResponse({
+      res,
+      message: 'edit a category',
+      body: category
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+      `[Error retrieving categories] - [index - PUT]: ${error.message}`
+    )
+    next(httpError)
+  }
+})
+
+module.exports = { createCategory, getCategoryById, getCategories, editCategory }
