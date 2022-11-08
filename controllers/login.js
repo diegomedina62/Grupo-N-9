94de7c7ad1382e96a8f53d0128b112ed4c35276a
Juan Becerra <juanpbrizo@gmail.com>
2022-11-08 16:21:38 -0500

    Moving the encode middleware functions to the postLogin controller

--- controllers/login.js
+++ controllers/login.js
@@ -1,23 +1,43 @@
 const createHttpError = require('http-errors')
+const bcryptjs = require('bcryptjs')
+
+const { User } = require('../database/models')
+
+const validationDb = require('../helpers/validationDb')
 const { endpointResponse } = require('../helpers/success')
 const { catchAsync } = require('../helpers/catchAsync')
+const { encode } = require('../helpers/jwtFuntions')
+const { ErrorObject } = require('../helpers/error')
 
 const postLogin = catchAsync(async (req, res, next) => {
-    try {
-        const response = { token: req.token }
+  const { email, password } = req.body
+  try {
+    const schema = { where: { email } }
+    const user = await validationDb(schema, User, true)
 
-        endpointResponse({
-            res,
-            message: 'Post Login',
-            body: response
-        })
-    } catch (error) {
-        const httpError = createHttpError(
-            error.statusCode,
-            `[User Not Found] - [index - POST]: ${error.message}`
-        )
-        next(httpError)
+    const compare = bcryptjs.compareSync(password, user.password)
+
+    if (!compare) {
+      const httpError = new ErrorObject('Invalid User or Password', 404)
+      next(httpError)
     }
+
+    const payload = { id: user.id }
+    const token = await encode(payload)
+    const response = { token }
+
+    endpointResponse({
+      res,
+      message: 'Post Login',
+      body: response
+    })
+  } catch (error) {
+    const httpError = createHttpError(
+      error.statusCode,
+            `[User Not Found] - [index - POST]: ${error.message}`
+    )
+    next(httpError)
+  }
 })
 
 module.exports = postLogin
--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -1,35 +1,6 @@
 const jwt = require('jsonwebtoken')
-const bcryptjs = require('bcryptjs')
 const { User } = require('../database/models')
-const tokenCreator = require('../helpers/tokenCreator')
 const { ErrorObject } = require('../helpers/error')
-
-const encode = async (req, res, next) => {
-  const { email, password } = req.body
-  try {
-    const user = await User.findOne({ where: { email } })
-
-    if (user === null) {
-      const httpError = new ErrorObject('Invalid User or Password', 404)
-      next(httpError)
-    }
-
-    const compare = bcryptjs.compareSync(password, user.password)
-
-    if (!compare) {
-      const httpError = new ErrorObject('Invalid User or Password', 404)
-      next(httpError)
-    }
-    const payload = { id: user.id }
-    const token = await tokenCreator(payload)
-    req.token = token
-
-    next()
-  } catch (error) {
-    const httpError = new ErrorObject(error.message, error.statusCode)
-    next(httpError)
-  }
-}
 
 const decode = async (req, res, next) => {
   const token = req.headers['x-access-token']
@@ -57,4 +28,4 @@ const decode = async (req, res, next) => {
   next()
 }
 
-module.exports = { encode, decode }
+module.exports = { decode }
