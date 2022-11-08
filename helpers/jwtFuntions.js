aba2c9c773a0626edd2f7e5bbd60c55b8b440a67
Juan Becerra <juanpbrizo@gmail.com>
2022-11-08 16:09:56 -0500

    Bundling JWT token encoding and decoding functions into jwtFuntions helpers

--- helpers/jwtFuntions.js
+++ helpers/jwtFuntions.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 const { ErrorObject } = require('./error')
 
-const tokenCreator = (payload) => {
+const encode = (payload) => {
   return new Promise((resolve, reject) => {
     jwt.sign(payload, process.env.SECRETORPRIVATEKEY, {
       expiresIn: '4h'
@@ -15,4 +15,13 @@ const tokenCreator = (payload) => {
   })
 }
 
-module.exports = tokenCreator
+const decoded = (token) => {
+  try {
+    const payload = jwt.verify(token, process.env.SECRETORPRIVATEKEY)
+    return payload
+  } catch (error) {
+    throw new ErrorObject('Invalid Token', 403)
+  }
+}
+
+module.exports = { encode, decoded }
